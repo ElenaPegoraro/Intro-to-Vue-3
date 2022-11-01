799ab1d755c2ae504ed3a355d96d897dff2ccf0d
Elena Pegoraro <elenapoikilia@gmail.com>
2022-11-01 19:06:39 +0100

    conditional

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+        product: 'Socks',
+        image: './assets/images/socks_blue.jpg',
+        url: "'https://www.vuemastery.com/'",
+        inventory: 10,
+        onSale: true
+    }
+}
+})
