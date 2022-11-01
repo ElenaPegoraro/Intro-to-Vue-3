036c9e2940890ead8cfb048d700ce34ed52e5b5f
Elena Pegoraro <elenapoikilia@gmail.com>
2022-11-01 21:00:56 +0100

    adding event

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const app = Vue.createApp({
   data() {
-    return {
-        product: 'Socks',
-        image: './assets/images/socks_blue.jpg',
-        url: "'https://www.vuemastery.com/'",
-        inventory: 10,
-        onSale: true,
-        sizes: ['XS','S', 'M', 'L', 'XL']
-    }
+      return {
+          cart:0,
+          product: 'Socks',
+          image: './assets/images/socks_blue.jpg',
+          inStock: true,
+          details: ['50% cotton', '30% wool', '20% polyester'],
+          variants: [
+            { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+            { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+          ]
+      }
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+}
 }
 })
