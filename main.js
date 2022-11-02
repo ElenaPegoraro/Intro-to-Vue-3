8bd46c3ef18ea70729ec31e2f35c79fd3575ae7d
Elena Pegoraro <elenapoikilia@gmail.com>
2022-11-02 10:57:14 +0100

    added events to buttons

--- main.js
+++ main.js
@@ -15,6 +15,12 @@ const app = Vue.createApp({
   methods: {
     addToCart() {
       this.cart += 1
-}
+},
+    updateImage(variantImage){
+      this.image = variantImage
+},
+    subtractFromCart() {
+      this.cart -= 1
+    }
 }
 })
